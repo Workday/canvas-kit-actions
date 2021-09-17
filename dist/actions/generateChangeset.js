91797d41cde5f86ee8e61486f699aff1ca6ebb50
NicholasBoll <nicholas.boll@gmail.com>
2021-09-17 19:51:16 +0000

    Deploying to v1 from @ Workday/canvas-kit-actions@92f282bd6a02ba7ebcbbdaec6d34c3c825594da7 🚀

---
+++ dist/actions/generateChangeset.js
@@ -0,0 +1,26 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const lib_1 = require("../lib");
+const repo_1 = require("../repo");
+const utils_1 = require("../utils");
+async function run() {
+    try {
+        const token = lib_1.actionsCore.getInput('token');
+        const fromRef = lib_1.actionsCore.getInput('fromRef');
+        const toRef = lib_1.actionsCore.getInput('toRef');
+        const tagName = lib_1.actionsCore.getInput('tagName');
+        const { owner, repo } = lib_1.actionsGithub.context.repo;
+        const api = (0, repo_1.getRepo)({ token, owner, repo });
+        const commits = await api.getCommits({ base: fromRef, head: toRef });
+        const { title, body, date } = (0, utils_1.getReleaseNotes)(owner, repo, commits, tagName);
+        lib_1.actionsCore.setOutput('title', title);
+        lib_1.actionsCore.setOutput('body', body);
+        lib_1.actionsCore.setOutput('date', date);
+    }
+    catch (e) {
+        if (e instanceof Error) {
+            lib_1.actionsCore.setFailed(e.message);
+        }
+    }
+}
+run();
---
+++ dist/utils.js
@@ -0,0 +1,243 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.enableAutoMerge = exports.getChangelogEntry = exports.getReleaseNotes = exports.getReleaseTitle = exports.getDate = exports.getReleaseCommitTitle = exports.getCommitParts = exports.getSections = exports.verifyPullRequest = exports.getMergeData = exports.getCommitBody = void 0;
+/**
+ * Creates a commit message out of Sections. This commit message will be processed later for changelog and release note
+ */
+function getCommitBody(sections) {
+    return `${sections.summary || ''}
+
+${sections['release category'] ? `[category:${sections['release category']}]\n\n` : 'Components'}
+
+${sections['release note'] ? `Release Note:\n${sections['release note']}\n\n` : ''}
+
+${sections['breaking changes'] ? `### BREAKING CHANGES\n${sections['breaking changes']}` : ''}
+`
+        .replace(/\n{2,}/g, '\n\n')
+        .trim();
+}
+exports.getCommitBody = getCommitBody;
+/**
+ * Determine auto merge data from a PR
+ */
+function getMergeData(prData) {
+    var _a;
+    const { title, number, body, headRefName } = ((_a = prData.repository) === null || _a === void 0 ? void 0 : _a.pullRequest) || {};
+    return {
+        title: (headRefName === null || headRefName === void 0 ? void 0 : headRefName.startsWith('merge/')) ? title : `${title} (#${number})`,
+        message: getCommitBody(getSections(body || '')),
+        strategy: (headRefName === null || headRefName === void 0 ? void 0 : headRefName.startsWith('merge/')) ? 'merge' : 'squash',
+    };
+}
+exports.getMergeData = getMergeData;
+function verifyPullRequest(prData) {
+    var _a;
+    const { title, body, headRefName } = ((_a = prData.repository) === null || _a === void 0 ? void 0 : _a.pullRequest) || {};
+    if (headRefName === null || headRefName === void 0 ? void 0 : headRefName.startsWith('merge/')) {
+        return false;
+    }
+    if (!body) {
+        return 'verifyPullRequest: No pull request body found';
+    }
+    const sections = getSections(body);
+    if (!sections.summary) {
+        return 'verifyPullRequest: No Summary section provided. Be sure the pull request description contains a `## Summary` section';
+    }
+    if (!sections['release category']) {
+        return 'verifyPullRequest: No Category section provided. Be sure the pull request description contains a `## Category` section for release notes';
+    }
+    if ((title === null || title === void 0 ? void 0 : title.startsWith('feat')) && !(headRefName === null || headRefName === void 0 ? void 0 : headRefName.startsWith('prerelease'))) {
+        return 'verifyPullRequest: All features should target a prerelease branch. Please update the base of the pull request to a prerelease branch.';
+    }
+    if (sections['breaking changes'] && !(headRefName === null || headRefName === void 0 ? void 0 : headRefName.startsWith('prerelease'))) {
+        return 'verifyPullRequest: All breaking changes should target a prerelease branch. Please update the base of the pull request to a prerelease branch or remove the breaking change.';
+    }
+    return false;
+}
+exports.verifyPullRequest = verifyPullRequest;
+function isValidHeading(input) {
+    return ['summary', 'release note', 'release category', 'breaking changes'].includes(input);
+}
+function getSections(input) {
+    let activeSection = '';
+    const sections = input
+        .replace(/\r/g, '')
+        .split('\n')
+        .reduce((result, line) => {
+        const headingMatch = line.match(/^#+\s+(.+)/);
+        const badgeMatch = line.match(/^!\[[a-z]+\]\(https:\/\/img.shields.io\/badge\/([a-z_]+)-([a-z_]+)-[a-z]+\)/i);
+        if (headingMatch) {
+            const heading = headingMatch[1].trim().toLowerCase();
+            if (isValidHeading(heading)) {
+                activeSection = heading;
+                result[activeSection] = '';
+            }
+            else {
+                // Deactivate active section until a new one is found
+                activeSection = '';
+            }
+        }
+        else if (badgeMatch) {
+            const heading = badgeMatch[1].replace(/_/g, ' ').trim();
+            const value = badgeMatch[2].replace(/_/g, ' ').trim();
+            if (isValidHeading(heading)) {
+                result[heading] = value;
+                // we're done with this heading, deactivate the section
+                activeSection = '';
+            }
+        }
+        else {
+            // A horizontal rule will break out of a section
+            if (line.trim() === '---') {
+                activeSection = '';
+            }
+            if (activeSection) {
+                result[activeSection] += `\n${line}`;
+            }
+        }
+        return result;
+    }, {});
+    // trim sections and remove comments
+    for (const [key, value] of Object.entries(sections)) {
+        sections[key] = value
+            .replace(/<!--[\s\S]+?-->/gm, '') // replace comments non-greedily in multiline mode
+            .replace(/[\n]{2,}/g, '\n\n') // assume more than 2 newlines in a row are a mistake, perhaps from removing comments
+            .trim();
+    }
+    return sections;
+}
+exports.getSections = getSections;
+function getCommitParts(input) {
+    const lines = input.replace(/\r/g, '').split('\n');
+    const firstLine = lines[0].trim();
+    const matches = firstLine.match(/(.+?)\s\(#([0-9]+)\)/);
+    // capture title and pull request number
+    const [_, title, pull_request] = matches || ['', firstLine, ''];
+    // capture category
+    let category = lines.reduce((result, line) => {
+        const matches = line.match(/\[category:(.+)]/);
+        if (matches) {
+            return matches[1];
+        }
+        return result;
+    }, '');
+    if (!category) {
+        // A category is not specified. Try to guess based on the type
+        if (title.startsWith('ci:')) {
+            category = 'Infrastructure';
+        }
+        else if (title.startsWith('docs:')) {
+            category = 'Documentation';
+        }
+        else {
+            // Fall back to 'Components'
+            category = 'Components';
+        }
+    }
+    // capture notes and breaking changes
+    let activeSection = '';
+    const sections = lines.reduce((result, line) => {
+        if (line.startsWith('Release Note:')) {
+            activeSection = 'release note';
+            result[activeSection] = '';
+        }
+        else if (line.startsWith('### BREAKING CHANGE')) {
+            activeSection = 'breaking change';
+            result[activeSection] = '';
+        }
+        else {
+            if (activeSection) {
+                result[activeSection] += `\n${line}`;
+            }
+        }
+        return result;
+    }, {});
+    // trim sections and remove comments
+    for (const [key, value] of Object.entries(sections)) {
+        sections[key] = value
+            .replace(/<!--[\s\S]+?-->/gm, '') // replace comments non-greedily in multiline mode
+            .replace(/[\n]{2,}/g, '\n\n') // assume more than 2 newlines in a row are a mistake, perhaps from removing comments
+            .trim();
+    }
+    return { title, pull_request, category, ...sections };
+}
+exports.getCommitParts = getCommitParts;
+function getReleaseCommitTitle(input, owner, repo, login) {
+    const baseUrl = 'https://github.com';
+    // - fix: Add crossorigin to font preloads [#967](https://github.com/Workday/canvas-kit/pull/967) [@NicholasBoll](https://github.com/NicholasBoll)
+    return `${input.title}${input.pull_request ? ` (${getPRLink(owner, repo, input.pull_request)})` : ''} ([@${login}](${baseUrl}/${login}))`;
+}
+exports.getReleaseCommitTitle = getReleaseCommitTitle;
+function getPRLink(owner, repo, pull_request) {
+    return `[#${pull_request}](https://github.com/${owner}/${repo}/pull/${pull_request})`;
+}
+function getDate() {
+    return new Date().toISOString().split('T')[0];
+}
+exports.getDate = getDate;
+function getReleaseTitle(owner, repo, tagName) {
+    return `## [${tagName}](https://github.com/${owner}/${repo}/releases/tag/${tagName}) (${getDate()})`;
+}
+exports.getReleaseTitle = getReleaseTitle;
+function getReleaseNotes(owner, repo, commits, tagName) {
+    const notes = [];
+    const breaking = [];
+    const categories = {};
+    commits.forEach(commit => {
+        var _a, _b;
+        const commitParts = getCommitParts(commit.commit.message);
+        // don't include dependabot or merge commits
+        if (((_a = commit.author) === null || _a === void 0 ? void 0 : _a.login) === 'dependabot[bot]' ||
+            commit.commit.message.startsWith('Merge') ||
+            commit.commit.message.startsWith('chore: Merge') ||
+            commit.commit.message.startsWith('chore: Release')) {
+            return;
+        }
+        let title = getReleaseCommitTitle(commitParts, owner, repo, ((_b = commit.author) === null || _b === void 0 ? void 0 : _b.login) || '');
+        if (commitParts['release note']) {
+            title += `\n${commitParts['release note']
+                .split('\n')
+                .map(line => `  ${line}`) // add 2 spaces to extra lines to make them line up with the list item in markdown
+                .join('\n')}`;
+        }
+        categories[commitParts.category] = `${categories[commitParts.category] || ''}\n- ${title}`;
+        if (commitParts['breaking change']) {
+            commitParts['breaking change'] //?
+            ;
+            `- ${commitParts.pull_request ? getPRLink(owner, repo, commitParts.pull_request) + ' ' : ''}${commitParts['breaking change']
+                .trim()
+                .split('\n')
+                .map(line => `  ${line}`) // add 2 spaces to extra lines to make them line up with the list item in markdown
+                .join('\n')}`;
+            breaking.push(`- ${commitParts.pull_request ? getPRLink(owner, repo, commitParts.pull_request) + ' ' : ''}${commitParts['breaking change']
+                .trim()
+                .split('\n')
+                .map((line, i) => (i === 0 ? line : `  ${line}`)) // add 2 spaces to extra lines to make them line up with the list item in markdown
+                .join('\n')}`);
+        }
+    });
+    let body = '';
+    if (breaking.length) {
+        body += `### BREAKING CHANGES\n\n${breaking.join('\n')}\n\n`;
+    }
+    body += Object.keys(categories)
+        .sort()
+        .map(key => {
+        return `### ${key}\n\n${categories[key].trim()}`;
+    })
+        .join('\n\n');
+    if (notes.length) {
+        body += `\n\n### Notes\n\n${notes.join('\n')}`;
+    }
+    return { date: getDate(), title: getReleaseTitle(owner, repo, tagName), body };
+}
+exports.getReleaseNotes = getReleaseNotes;
+function getChangelogEntry(owner, repo, commits, tagName) {
+    const { title, body } = getReleaseNotes(owner, repo, commits, tagName);
+    return `${title}\n\n${body}`;
+}
+exports.getChangelogEntry = getChangelogEntry;
+function enableAutoMerge(prData) {
+    return 'merge';
+}
+exports.enableAutoMerge = enableAutoMerge;
