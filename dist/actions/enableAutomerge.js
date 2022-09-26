814eb8c50ae5ed1ac2437497991f638bab63ecdd
NicholasBoll <nicholas.boll@gmail.com>
2022-09-26 23:01:56 +0000

    Deploying to v1 from @ Workday/canvas-kit-actions@c495b2489204c6a73838b9c6b2f8f772457801fb 🚀

--- dist/actions/enableAutomerge.js
+++ dist/actions/enableAutomerge.js
@@ -4,7 +4,7 @@ const lib_1 = require("../lib");
 const repo_1 = require("../repo");
 const utils_1 = require("../utils");
 async function run() {
-    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k;
+    var _a, _b, _c, _d, _e, _f, _g, _h, _j, _k, _l, _m;
     const token = lib_1.actionsCore.getInput('token');
     const number = lib_1.actionsCore.getInput('number');
     const repo = (0, repo_1.getRepo)({ token, ...lib_1.actionsGithub.context.repo });
@@ -48,7 +48,8 @@ async function run() {
         else if (typeof e === 'string') {
             lib_1.actionsCore.info(`\nMessage: ${e}`);
         }
-        if (((_h = (_g = prData.repository) === null || _g === void 0 ? void 0 : _g.pullRequest) === null || _h === void 0 ? void 0 : _h.mergeable) === 'MERGEABLE') {
+        if (((_h = (_g = prData.repository) === null || _g === void 0 ? void 0 : _g.pullRequest) === null || _h === void 0 ? void 0 : _h.mergeable) === 'MERGEABLE' &&
+            prData.repository.pullRequest.mergeStateStatus === 'CLEAN') {
             // Automerge failed. Try a straight merge
             await repo.merge({
                 pullRequestId: id,
@@ -57,7 +58,7 @@ async function run() {
         }
         else {
             // PR is not mergeable
-            throw new Error(`Pull request is not mergeable. Github mergeability: ${(_k = (_j = prData.repository) === null || _j === void 0 ? void 0 : _j.pullRequest) === null || _k === void 0 ? void 0 : _k.mergeable}`);
+            throw new Error(`Pull request is not mergeable. Github mergeability: ${(_k = (_j = prData.repository) === null || _j === void 0 ? void 0 : _j.pullRequest) === null || _k === void 0 ? void 0 : _k.mergeable}. Github Merge status: ${(_m = (_l = prData.repository) === null || _l === void 0 ? void 0 : _l.pullRequest) === null || _m === void 0 ? void 0 : _m.mergeStateStatus}`);
         }
     }
     lib_1.actionsCore.info(`Enabled Github's automerge feature. Success!`);
