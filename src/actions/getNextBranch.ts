9b5a684dfc13859e92e6b4c1429f671f73d67056
Raisa Primerova <48605821+RayRedGoose@users.noreply.github.com>
2023-11-28 14:48:51 -0700

    fix: Set default as master

--- src/actions/getNextBranch.ts
+++ src/actions/getNextBranch.ts
@@ -2,7 +2,7 @@ import {actionsCore as core, actionsGithub as github} from '../lib'
 import {getNextBranch} from '../utils'
 
 async function run() {
-  const mainBranch = core.getInput('mainBranch')
+  const mainBranch = core.getInput('mainBranch') || 'master'
   const branch = core.getInput('branch') || github.context.ref.replace('refs/heads/', '')
 
   core.setOutput('branch', getNextBranch(branch, mainBranch))
